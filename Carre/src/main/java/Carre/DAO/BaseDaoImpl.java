74ab11f6e4381facecbe76e70e5d98b6aab60e1b
ESMITS <Evert.Smits@hva.nl>
2016-10-11 14:56:55 +0200

    dao klein dingetje

--- Carre/src/main/java/Carre/DAO/BaseDaoImpl.java
+++ Carre/src/main/java/Carre/DAO/BaseDaoImpl.java
@@ -9,11 +9,15 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Evert
  */
+@Repository
+@Transactional
 public class BaseDaoImpl implements BaseDao {
     @PersistenceContext
     private EntityManager em;
